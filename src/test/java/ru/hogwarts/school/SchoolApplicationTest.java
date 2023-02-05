15470b316b386665331b518ebd1ae9be703b9d74
nikonovdmitiy <nikonov9094@gmail.com>
2023-02-05 12:53:34 +0500

    hw

--- src/test/java/ru/hogwarts/school/SchoolApplicationTest.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTest.java
@@ -12,7 +12,7 @@ import ru.hogwarts.school.model.Student;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
-class Hogwarts2ApplicationTests {
+class SchoolApplicationTest {
 
     @LocalServerPort
     private int port;
